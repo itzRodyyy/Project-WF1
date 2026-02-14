e6aafefbf2c891ee322363e2bc7394fc8134c18f
itzRodyyy <rodydevs@gmail.com>
2026-02-14 14:07:15 -0800

    Refactoring to allow for Inventory Update
    + Added Weapon Scripatable Object
    + Changed Player Model a tad

--- Witherfade/Assets/Scripts/Items/Weapon.cs
+++ Witherfade/Assets/Scripts/Items/Weapon.cs
@@ -3,5 +3,8 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Weapon", menuName = "Scriptable Objects/Item/Weapon")]
 public class Weapon : ScriptableObject
 {
-    public 
+    public GameObject model;
+    [Range(1, 10)] int weaponDmg;
+    [Range(0, 3)] float attackRate;
+    [Range(1, 50)] int weaponRange;
 }
